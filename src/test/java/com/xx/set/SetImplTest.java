c381ea19d264fce711eb1d7f32aebf2c28bd1782
cihanuluisik <cihanuluisik@hotmail.com>
2017-11-09 18:53:20 +0000

    test 2 step 3 refactoring : refactor some

--- src/test/java/com/xx/set/SetImplTest.java
+++ src/test/java/com/xx/set/SetImplTest.java
@@ -1,28 +1,34 @@
 package com.xx.set;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetImplTest {
 
 
+    private Set set;
+
+    @Before
+    public void setUp() throws Exception {
+        set = new SetImpl();
+    }
+
     @Test
     public void givenAnewSetSizeMustBe0() throws Exception {
-        Set set = new SetImpl();
         assertThat(set).hasSize(0);
     }
 
 
     @Test
     public void givenAnewSetWithOneElementSizeMustBe1() throws Exception {
-        Set set = new SetImpl();
         set.add("Item1");
         assertThat(set).hasSize(1);
     }
+
 
 
 
