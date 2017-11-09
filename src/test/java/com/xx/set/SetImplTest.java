589acbea1377de3788f35c03b7d601c98ffb5642
cihanuluisik <cihanuluisik@hotmail.com>
2017-11-09 18:38:43 +0000

    step 1 : first set with no class created yet

--- src/test/java/com/xx/set/SetImplTest.java
+++ src/test/java/com/xx/set/SetImplTest.java
@@ -1,12 +1,19 @@
 package com.xx.set;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetImplTest {
 
 
     @Test
     public void givenAnewSetSizeMustBe0() throws Exception {
+        Set set = new SetImpl();
+        assertThat(set).hasSize(0);
 
     }
 
